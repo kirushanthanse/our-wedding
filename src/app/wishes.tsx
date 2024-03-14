781f749003f5c756c0b81fc4968dd69c6e5d2756
kirushanthanse <krishanthan000@gmail.com>
2024-03-14 11:25:00 +0530

    after pic change

--- src/app/wishes.tsx
+++ src/app/wishes.tsx
@@ -80,7 +80,7 @@ function Wishes() {
           </Button>
         </div>
         <div className='picture'>
-          <img className="bd-placeholder-img rounded-circle us-img" width="220" height="220" src='./og.jpg' alt='couple picture'/>
+          <img className="bd-placeholder-img rounded-circle us-img" width="220" height="220" src='./us.jpg' alt='couple picture'/>
         </div>
         <div className='text'>
           <h2 id='h2'>SAVE THE DATE ❤️</h2>
